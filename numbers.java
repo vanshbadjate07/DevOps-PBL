c29f1def6568526b429a6947551a18bffc2130c2
Vansh <badjatevansh1008@gmail.com>
2024-12-30 11:56:03 +0530

    Second Commit

--- numbers.java
+++ numbers.java
@@ -1,6 +1,7 @@
 // Java Task: Write a Java program that prints the numbers from 1 to 10 to the console.
 class numbers{
     public static void main(String args[]){
+        System.out.println("Printing 1 to 10");
         for(int i = 1;i<=10;i++){
             System.out.println(i);
         }
